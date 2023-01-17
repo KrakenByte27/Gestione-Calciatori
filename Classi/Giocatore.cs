576271e62b4f9199e12631e03009bcdcfeb64388
santo <santo@PC-FISSO>
2023-01-17 07:24:20 +0100

    Aggiungere i file di progetto.

---
+++ Classi/Giocatore.cs
@@ -0,0 +1,19 @@
+﻿namespace Gestione_Calciatori.Classi
+{
+    class Giocatore
+    {
+        public Giocatore(string nome1, string squadra1, int partite1, int goal1, Giocatore next1)
+        {
+            Nome = nome1;
+            Squadra = squadra1;
+            Partite = partite1;
+            Goal = goal1;
+            Next = next1;
+        }
+        public string Nome { get; set; }
+        public string Squadra { get; set; }
+        public int Goal { get; set; }
+        public int Partite { get; set; }
+        public Giocatore Next { get; set; }
+    }
+}
